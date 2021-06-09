7d62637aa5b1187901548195ee847b4cfe3f3a57
morganoconnor-hp <morgan.oconnor@humapro.fr>
2021-06-09 12:57:02 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useState} from "react";
 
 function App() {
-  return (
+
+    const [computers, setComputers] = useState();
+
+    axios.get('http://localhost:1030/api/computers')
+        .then(res => {
+            setComputers(res.data);
+        })
+
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {( computers ) &&
+              computers.map(computer =>
+                  <div>{computer.name}</div>
+              )
+          }
       </header>
     </div>
   );
